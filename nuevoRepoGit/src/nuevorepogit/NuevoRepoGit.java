a87627ee6f19692aed47f4349acd650b7427062f
iban <iban@iban-HP-Pavilion-g6-Notebook-PC>
2015-05-18 13:20:01 +0200

    otro

--- nuevoRepoGit/src/nuevorepogit/NuevoRepoGit.java
+++ nuevoRepoGit/src/nuevorepogit/NuevoRepoGit.java
@@ -11,6 +11,8 @@ public class NuevoRepoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //cambio que hago ahora
         System.out.println("cambio de pruebas realizado"); 
            System.out.println("cambio de pruebas realizado");
            System.out.println("cambio de pruebas realizado");
