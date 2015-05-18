cde708ec36b436f765f937b1ea2def70a3d4c245
iban <iban@iban-HP-Pavilion-g6-Notebook-PC>
2015-05-18 12:26:54 +0200

    otro cambio para probar

--- nuevoRepoGit/src/nuevorepogit/NuevoRepoGit.java
+++ nuevoRepoGit/src/nuevorepogit/NuevoRepoGit.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package nuevorepogit;
+
+
 /**
  *
  * @author iban
@@ -13,7 +11,6 @@ public class NuevoRepoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("cambio de pruebas realizado");  }
     
 }
